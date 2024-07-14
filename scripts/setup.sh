cb87b0554542d456e0692f2b961af588599ab7f1
zwoefler <>
2024-07-14 19:58:29 +0200

    Adding VSCode

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -78,6 +78,24 @@ else
 fi
 
 echo "############################"
+echo "VSCODE"
+echo "############################"
+
+echo "[VSCODE] Add Microsoft gpg key to install VSCode"
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+rm -f packages.microsoft.gpg
+
+echo "[VSCODE] Installing VSCode"
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
+
+echo ""
+
+echo "############################"
 echo "# VMCHAMP"
 echo "############################"
 check_CPU_supports_virtualisation() {
