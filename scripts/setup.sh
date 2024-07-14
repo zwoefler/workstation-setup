9d06735b4731d173bf4b9bf957e30ab5a8475bd8
zwoefler <>
2024-07-14 17:52:00 +0200

    Updating script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,6 +12,11 @@ else
     user_home=$HOME
 fi
 
+
+echo ""
+echo "##################"
+echo "[APT]"
+echo "##################"
 update_apt_cache() {
     local last_update=$(stat -c %Y /var/cache/apt/pkgcache.bin)
     local now=$(date +%s)
@@ -20,7 +25,7 @@ update_apt_cache() {
     let elapsed=now-last_update
     if [ "$elapsed" -gt 3600 ]; then
         echo "[APT] Running apt update..."
-        apt update
+        sudo apt update
     else
         echo "[APT] Skipping apt update, performed recently."
     fi
@@ -29,17 +34,20 @@ update_apt_cache() {
 update_apt_cache
 
 echo "[APT] Upgrading packages..."
-apt-get upgrade -y
+sudo apt upgrade -y
 
 echo "[APT] Remove packages..."
-apt-get remove -y nano
+sudo apt remove -y nano
 
 echo "[APT] Installing packages..."
-apt-get install -y vim git jq tree python3-pip firefox-esr openssh-client ffmpeg
+sudo apt install -y vim git jq tree python3-pip firefox-esr openssh-client ffmpeg
 
-############################
-# CREATE SSH KEYS
-############################
+echo "######################"
+echo ""
+
+echo "############################"
+echo "[SSH] CREATE SSH KEYS"
+echo "############################"
 if systemctl is-active --quiet ssh; then
     echo "[SSH] SSH service active."
 else
@@ -57,15 +65,17 @@ if [ ! -f "$user_home/.ssh/github_rsa" ]; then
     su -c "ssh-keygen -t rsa -b 2048 -f $user_home/.ssh/github_rsa -N ''" -s /bin/sh $SUDO_USER
 fi
 
-############################
-# .BASHRC
-############################
+echo "##########################"
+echo ""
+
+echo "############################"
+echo "# .BASHRC"
+echo "############################"
 script_path=$(dirname "$0")
 local_bashrc="${script_path}/dot-files/.bashrc"
 remote_url="https://raw.githubusercontent.com/zwoefler/workstation-setup/master/dot-files/.bashrc"
 bashrc_path="$user_home/.bashrc"
 
-# TODO: If exists skip
 if [ -f "$local_bashrc" ]; then
     echo "[DOT-FILES] Use local .bashrc"
     cp "$local_bashrc" "$bashrc_path"
@@ -74,12 +84,12 @@ else
     curl -sLo "$bashrc_path" "$remote_url"
 fi
 
-############################
-# VMCHAMP
-############################
+echo "############################"
+echo "# VMCHAMP"
+echo "############################"
 check_CPU_supports_virtualisation() {
     if ! egrep -c '(vmx|svm)' /proc/cpuinfo &> /dev/null; then
-        echo "ERROR: Your CPU does not support virtualization."
+        echo "[VMCHAMP] ERROR: Your CPU does not support virtualization."
         exit 1
     fi
 }
@@ -88,14 +98,14 @@ install_vmchamp() {
     echo "[VMCHAMP] Downloading and installing VmChamp..."
     wget -qO- https://api.github.com/repos/zwoefler/VmChamp/releases/latest | grep "browser_download_url" | cut -d '"' -f 4 | wget -i - -O vmchamp
     chmod +x vmchamp
-    mv vmchamp /usr/local/bin/
+    sudo mv vmchamp /usr/local/bin/
     echo "[VMCHAMP] VmChamp installed successfully."
 
     echo "[VMCHAMP] Checking requirements for KVM and libvirt..."
     if ! dpkg -l | grep -qw qemu-kvm; then
         echo "[VMCHAMP] KVM is not installed. Installing..."
-        apt update
-        apt install -y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils
+        sudo apt update
+        sudo apt install -y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils
         echo "[VMCHAMP] KVM and related packages installed."
     else
         echo "[VMCHAMP] KVM is already installed."
@@ -111,13 +121,12 @@ if ! command -v vmchamp &> /dev/null; then
     check_CPU_supports_virtualisation
     install_vmchamp
 else
-    echo "VmChamp is already installed."
+    echo "[VMCHAMP] VmChamp is already installed."
 fi
 
-###################
-# NERDCTL
-###################
-#TODO: IS INSTALLED AS ROOT USER....
+echo "###################"
+echo "# NERDCTL"
+echo "###################"
 NERDCTL_VERSION=1.7.6
 
 install_nerdctl() {
@@ -125,22 +134,13 @@ install_nerdctl() {
     echo "[NERDCTL] Checking requirement uidmap package is installed"
     if ! dpkg -l | grep -q uidmap; then
         echo "[NERDCTL] uidmap not installed. Installing..."
-        apt update
-        apt install -y uidmap
+        sudo apt install -y uidmap
     fi
 
     echo "[NERDCTL] Installing nerdctl"
     wget https://github.com/containerd/nerdctl/releases/download/v$NERDCTL_VERSION/nerdctl-full-$NERDCTL_VERSION-linux-amd64.tar.gz
-    tar Cxzvvf /usr/local nerdctl-full-$NERDCTL_VERSION-linux-amd64.tar.gz
-
-    if [ -n "$SUDO_USER" ]; then
-        echo "[NERDCTL] Running rootless setup as $SUDO_USER"
-        sudo -u $SUDO_USER containerd-rootless-setuptool.sh install
-    else
-        echo "[NERDCTL] Script not ran as sudo. Can not ensure correct user context for nerdctl setup."
-        exit 1
-    fi
-
+    sudo tar Cxzvvf /usr/local nerdctl-full-$NERDCTL_VERSION-linux-amd64.tar.gz
+    containerd-rootless-setuptool.sh install
     echo "[NERDCTL] Successfully installed NERDCTL"
 }
 
